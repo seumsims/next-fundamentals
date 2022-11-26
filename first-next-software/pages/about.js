8812d0dffca5a44a174452ca59b294b508f74b9c
seumsims <seumworldinbangladesh@gmail.com>
2022-11-26 20:15:44 +0600

    master first next soft seo strategy completed and add builtwith and trend built with dot com

--- first-next-software/pages/about.js
+++ first-next-software/pages/about.js
@@ -10,6 +10,15 @@ class About extends Component {
                     <title>
                         About
                     </title>
+
+                    <link rel="shortcut icon" href="https://react.rabbil.com/images/favicon.ico" type="image/x-icon"/>
+                    <link rel="icon" href="https://react.rabbil.com/images/favicon.ico" type="image/x-icon"/>
+                    <meta name="description"
+                          content="Laravel tutorial bangla | Rabbil Hasan is an Expert Web Developer in Bangladesh. He is highly talented, experienced, professional and cooperative software engineer, working in programming and web world for more than 4 years. Moreover Rabbil Hasan has a skilled team for achieving his goal named “Team Rabbil”.Team Rabbil assure you a wide range of quality IT services. "/>
+                    <meta name="keywords"
+                          content="Laravel  tutorial bangla, Expert Web Developer in Bangladesh, Expert Mobile App Developer in Bangladesh, Android App Developer in Bangladesh"/>
+                    <meta name="author" content="Rabbil Hasan"/>
+
                 </Head>
 
                 <ul>
--- first-next-software/pages/index.js
+++ first-next-software/pages/index.js
@@ -11,11 +11,15 @@ class Index extends Component {
                     <title>
                         Home
                     </title>
+
                     <link rel="shortcut icon" href="https://react.rabbil.com/images/favicon.ico" type="image/x-icon"/>
-                        <link rel="icon" href="https://react.rabbil.com/images/favicon.ico" type="image/x-icon"/>
-                            <meta name="description" content="Laravel tutorial bangla | Rabbil Hasan is an Expert Web Developer in Bangladesh. He is highly talented, experienced, professional and cooperative software engineer, working in programming and web world for more than 4 years. Moreover Rabbil Hasan has a skilled team for achieving his goal named “Team Rabbil”.Team Rabbil assure you a wide range of quality IT services. "/>
-                                <meta name="keywords" content="Laravel  tutorial bangla, Expert Web Developer in Bangladesh, Expert Mobile App Developer in Bangladesh, Android App Developer in Bangladesh"/>
-                                    <meta name="author" content="Rabbil Hasan"/>
+                    <link rel="icon" href="https://react.rabbil.com/images/favicon.ico" type="image/x-icon"/>
+                    <meta name="description"
+                          content="Laravel tutorial bangla | Rabbil Hasan is an Expert Web Developer in Bangladesh. He is highly talented, experienced, professional and cooperative software engineer, working in programming and web world for more than 4 years. Moreover Rabbil Hasan has a skilled team for achieving his goal named “Team Rabbil”.Team Rabbil assure you a wide range of quality IT services. "/>
+                    <meta name="keywords"
+                          content="Laravel  tutorial bangla, Expert Web Developer in Bangladesh, Expert Mobile App Developer in Bangladesh, Android App Developer in Bangladesh"/>
+                    <meta name="author" content="Rabbil Hasan"/>
+
                 </Head>
 
                 <ul>
@@ -37,12 +41,12 @@ class Index extends Component {
                     </li>
                 </ul>
 
-                <h1 style={{color:"limegreen"}}>Salamoon Alaikoom </h1>
+                <h1 style={{color: "limegreen"}}>Salamoon Alaikoom </h1>
                 <h1 style={{color: "palevioletred"}}>This is home page</h1>
                 <h1 style={{color: "lightsteelblue"}}>Welcome to Next JS 13</h1>
 
-                <Image src="/images/file.png" alt="me" width="680" height="384" />
-                <img src="/pictures/Ixora%20casei%20458kilo.png" alt="me" width="680" height="384" />
+                <Image src="/images/file.png" alt="me" width="680" height="384"/>
+                <img src="/pictures/Ixora%20casei%20458kilo.png" alt="me" width="680" height="384"/>
                 {/* this is not going to work */}
                 {/*<img src="./Ixora%20casei%20458kilo.png" alt="me" width="680" height="384" />*/}
             </div>
