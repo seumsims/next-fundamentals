1b8e7c34a2c82aefd81f7ecae4195c01e358eea5
seumsims <seumworldinbangladesh@gmail.com>
2022-11-26 14:36:59 +0600

    master first next soft index js rcc first start completed

--- first-next-software/pages/index.js
+++ first-next-software/pages/index.js
@@ -1,5 +1,13 @@
-function HomePage() {
-    return <div>Salamoon Alaikoom Welcome to Next.js!</div>
-  }
-  
-  export default HomePage
+import React, {Component} from 'react';
+
+class Index extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Salamoon Alaikoom Welcome to Next JS 13</h1>
+            </div>
+        );
+    }
+}
+
+export default Index;
